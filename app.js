e3aeb719f08e7f376eb306279028301b4698ea06
derek ney <deek311@gmail.com>
2022-12-10 00:27:50 -0700

    added functions

--- app.js
+++ app.js
@@ -6,37 +6,69 @@ console.log()
 
 
 //rendering task
-function renderTodos() {
+function rendercontacts() {
     editCategoryInput.hidden = true
-    let contactData = localStorage.getItem("TodoLocalStorage");
+    let contactData = localStorage.getItem("contactsLocalStorage");
     console.table(contactData)
     if (contactData == null) {
       textOutput = [];
     } else {
-      todos = JSON.parse(todoData);
+      contacts = JSON.parse(contactsData);
     }
     const pendingTasksNumb = document.querySelector(".tasksLeft");
-    pendingTasksNumb.textContent = todos.length;
-    if (todos.length > 0) {
+    pendingTasksNumb.textContent = contacts.length;
+    if (contacts.length > 0) {
       clearBtn.classList.add("active"); 
     } else {
       clearBtn.classList.remove("active"); 
     }
     let newLiTag = "";
-    todos.forEach((element, index) => {
+    contacts.forEach((element, index) => {
   
       if (element.category === filterByCategory || filterByCategory === undefined || refreshCategories) {
         if (element.editing) {
-          newLiTag += `<li><input type="text" class="editTodo"  placeholder=${element.text}><span class="icon" onclick="updateTodo(${index})"><i class="fa-solid fa-check"></i></span></li>`;
+          newLiTag += `<li><input type="text" class="editcontacts"  placeholder=${element.text}><span class="icon" onclick="updatecontacts(${index})"><i class="fa-solid fa-check"></i></span></li>`;
         } else if (element.completed) {
-          newLiTag += `<li onclick="completeTodo(${index})"><s>${element.text} ${element.category ? `(${element.category})` : ""}</s><span class="icon" onclick="deleteTodo(${index})"><i class="fas fa-trash"></i></span></li>`;
+          newLiTag += `<li onclick="completecontacts(${index})"><s>${element.text} ${element.category ? `(${element.category})` : ""}</s><span class="icon" onclick="deletecontacts(${index})"><i class="fas fa-trash"></i></span></li>`;
         } else {
-          newLiTag += `<li onclick="completeTodo(${index})">${element.text} ${element.category ? `(${element.category})` : ""}<span class="icon" onclick="editTodo(${index})"><i class="fas fa-edit"></i></span></li>`;
+          newLiTag += `<li onclick="completecontacts(${index})">${element.text} ${element.category ? `(${element.category})` : ""}<span class="icon" onclick="editcontacts(${index})"><i class="fas fa-edit"></i></span></li>`;
         }
       }
     });
-    todoList.innerHTML = newLiTag;
+    contactsList.innerHTML = newLiTag;
     if (refreshCategories) {
       renderCategories()
     }
+  } 
+
+  // delete function
+function deleteContact(index) {
+  let contactsData = localStorage.getItem("contactsLocalStorage");
+  contacts = JSON.parse(contactsData);
+  if (contacts[index].completed) {
+    contacts.splice(index, 1);
+    localStorage.setItem("contactsLocalStorage", JSON.stringify(contacts));
+    rendercontacts();
   }
+}
+
+
+// change UI to show edit around the contacts
+function updatecontacts(id) {
+  const text = document.querySelector(".editcontacts").value
+  contacts[id].editing = false
+  contacts[id].text = text
+  contacts[id].completed = false
+  localStorage.setItem("contactsLocalStorage", JSON.stringify(contacts));
+  rendercontacts()
+}
+
+// delete list
+clearBtn.onclick = () => {
+  contacts = []; 
+  localStorage.setItem("contactsLocalStorage", JSON.stringify(contacts)); 
+  rendercontacts(); 
+};
+
+// initial render
+rendercontacts();
